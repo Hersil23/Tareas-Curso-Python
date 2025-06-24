1d4c633665da3e21bc5b9e2bde3f4a58f361351d
Herasi Silva <herasidesweb@gmail.com>
2025-06-23 22:09:21 -0400

    1

--- Variables/ejercicios_variables.py
+++ Variables/ejercicios_variables.py
@@ -127,4 +127,9 @@ products = [
 
 print("Nombre del segundo producto:", products[1]["nombre"])
 
+# entrada de datos
+# input () nos permite capturar datos del usuario por consola, es decir permite la entrada
+# de datos a mi aplicacion 
 
+name2 = input("Ingrese su nombre: ")
+print("Hola", name2, "bienvenido a twistpro.net")
---
+++ Variables/variable.py
@@ -0,0 +1,9 @@
+# entrada de datos
+# input () nos permite capturar datos del usuario por consola, es decir permite la entrada
+# de datos a mi aplicacion 
+
+name2 = input("Ingrese su nombre: ")
+print("Hola", name2, "bienvenido a twistpro.net")
+
+age2 = int(input("Ingrese su edad: "))
+print("Tu edad es:", age2)
